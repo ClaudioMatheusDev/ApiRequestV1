71c269bc21afd0638a184d79b701a31766942387
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-01-30 22:52:56 -0300

    Adicionando endpoints

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,69 @@
+﻿using APIRequest.Context;
+using APIRequest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIRequest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if(produtos is null)
+            {
+                return NotFound("Produtos não encontrado!");
+            }
+            return produtos;
+        }
+        [HttpGet("{id:int}", Name="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p=>  p.ProdutoID == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado!");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if(produto is null)
+                return BadRequest("Produto não inserido!");
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoID }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoID)
+            {
+                return BadRequest("Produto não atualizado!");
+            }
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+            
+            return Ok("Produto atualizado com sucesso!");
+        }
+
+
+    }
+}
