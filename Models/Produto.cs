de2a39d4a4749f5d9d6101ac510fde2bad1469c6
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-02-11 23:15:52 -0300

    :construction: Usando data annotations

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -10,16 +10,17 @@ public class Produto
     [Key]
     public int ProdutoID { get; set; }
 
-    [Required]
-    [StringLength(80)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(15, ErrorMessage ="O nome deve ter entre 5 e 15 caracteres", MinimumLength = 5)]
     public string? Nome { get; set; }
 
     [Required]
-    [StringLength(300)]
+    [StringLength(10, ErrorMessage = "A descricao deve ter mo máximo {1} caracteres")]
     public string? Descricao { get; set; }
 
     [Required]
     [Column(TypeName="decimal(10,2)")]
+    [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
     public decimal Preco { get; set; }
 
     [Required]
